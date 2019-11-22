985b7d683c402afebcf25af803f66883ee2df02d
Ryan M <rmccauleymusic@gmail.com>
2019-11-22 01:33:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,6 +19,7 @@ class Transfer
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"
+      end
 
       elsif self.status == "complete"
         return "Transaction already completed."
