0ca2c8fa9f86a0446f1a0c7d1e55fc26d5985cff
Ryan M <rmccauleymusic@gmail.com>
2019-11-22 00:31:29 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,7 +13,12 @@ class BankAccount
     end
 
     def display_balance
-      puts "Your balance is $#{name.balance}."
+      puts "Your balance is $#{self.balance}."
     end
-
+    
+    def valid?
+      if self.status == "open" && self.balance > 0
+      end
+    end
+      
 end
